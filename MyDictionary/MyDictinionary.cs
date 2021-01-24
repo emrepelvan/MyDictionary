2f729115fca89f0fcc954c35a7007e34cfca960b
emrepelvan <emrepelvan@hotmail.com.tr>
2021-01-25 00:02:53 +0300

    Add project files.

---
+++ MyDictionary/MyDictinionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey,TValue>
+    {
+        TKey[] kArray;
+        TValue[] vArray;
+        TKey[] tempKArray;
+        TValue[] tempVArray;
+
+        public MyDictionary()     //constructor
+        {
+            kArray = new TKey[0];
+            vArray = new TValue[0];
+
+        }
+
+        public void Add(TKey key ,TValue value)
+        {
+            tempKArray = kArray;
+            tempVArray = vArray;
+
+            kArray = new TKey[kArray.Length + 1];
+            vArray = new TValue[vArray.Length + 1];
+            
+            for (int i = 0; i < tempKArray.Length; i++)
+            {
+                kArray[i] = tempKArray[i];
+            }
+
+            for (int i = 0; i < tempVArray.Length; i++)
+            {
+                vArray[i] = tempVArray[i];
+            }
+
+            kArray[kArray.Length - 1] = key;
+            vArray[vArray.Length - 1] = value;
+
+        }
+        public TKey[] Item1
+        {
+            get { return kArray; }
+        }
+        public TValue[] Item2
+        {
+            get { return vArray; }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> countries = new MyDictionary<string, string>();
+            
+            countries.Add("tr", "Turkey");
+            countries.Add("de", "Germany");
+            countries.Add("nl", "Netherlands");
+            countries.Add("az", "Azerbaijan");
+            countries.Add("uk", "United Kingdom");
+
+            for (int i = 0; i < countries.Item1.Length; i++)
+            {
+                Console.WriteLine( countries.Item1[i] + " : " + countries.Item2[i]);
+
+            }
+        }
+    }
+}
